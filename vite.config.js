bca59ac757fb5b828ee5a2f029b179ce284aff49
Akshay <akshay.sharma@udayattech.com>
2025-08-24 10:59:51 +0530

    Rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5175,
-    allowedHosts: [
-      '3e41e292ac49.ngrok-free.app',
-      'online.immigovaau.info'
-    ]
-  }
+  // server: {
+  //   host: true,
+  //   port: 5175,
+  //   allowedHosts: [
+  //     '3e41e292ac49.ngrok-free.app',
+  //     'online.immigovaau.info'
+  //   ]
+  // }
 })
